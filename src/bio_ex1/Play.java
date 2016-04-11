584104bc457335a12194d67b93f6830942e0b73d
shahaf <shahaf@minute.ly>
2016-04-12 00:08:40 +0300

    added prob to init board

--- src/bio_ex1/Play.java
+++ src/bio_ex1/Play.java
@@ -3,7 +3,7 @@ package bio_ex1;
 public class Play {
 		
 	public static void main(String[] args) {
-		Board game = new Board(100);
+		Board game = new Board(100, 0.9);
 		
 		for(int i = 0 ; i <= 0; i++){
 			game.step(i,true);
